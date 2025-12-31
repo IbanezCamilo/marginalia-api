8bf444b3a0fed02198c027c45ca504da773e00de
ImCamilo17 <Camilopastrana41@gmail.com>
2025-12-30 23:36:15 -0500

    fix image profile image error's

--- src/main/java/com/blog/blog_literario/controllers/profile/UserProfileController.java
+++ src/main/java/com/blog/blog_literario/controllers/profile/UserProfileController.java
@@ -1,17 +1,27 @@
 package com.blog.blog_literario.controllers.profile;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.BindingResult; //Jakarta para validaciones
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.blog.blog_literario.dto.profile.userProfileResponseDTO; // Anotaciones para crear controladores REST
 import com.blog.blog_literario.dto.profile.userProfileUpdateDTO;
+import com.blog.blog_literario.model.User;
+import com.blog.blog_literario.repositories.UserRepository;
+import com.blog.blog_literario.services.general.ImageStorageService;
 import com.blog.blog_literario.services.secundary.UserProfileService;
 
 import jakarta.validation.Valid;
@@ -22,7 +32,12 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/user/profile")
 public class UserProfileController {
 
-    private final UserProfileService userProfileService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ImageStorageService imageStorageService;
+    @Autowired
+    private UserProfileService userProfileService;
 
     @GetMapping
     // param: Obtiene el usuario Autenticado
@@ -52,5 +67,22 @@ public class UserProfileController {
             //status 500: internal error
             return ResponseEntity.status(500).body("Error al actualizar el perfil " + e.getMessage());
         }
+    }
+
+    @PostMapping("/upload-image")
+    public ResponseEntity<?> uploadProfileImage(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestParam("image") MultipartFile image) {
+
+        User user = userRepository.findByEmail(userDetails.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        String imageUrl = imageStorageService.saveImage(image);
+
+        user.setProfilePicture(imageUrl); //replace the image
+        userRepository.save(user);
+
+        return ResponseEntity.ok(Map.of("imageUrl", imageUrl));
+
     }
 }
--- src/main/java/com/blog/blog_literario/services/general/impl/PostServiceImpl.java
+++ src/main/java/com/blog/blog_literario/services/general/impl/PostServiceImpl.java
@@ -1,13 +1,7 @@
 package com.blog.blog_literario.services.general.impl;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,6 +17,7 @@ import com.blog.blog_literario.model.User;
 import com.blog.blog_literario.repositories.CategoryRepository;
 import com.blog.blog_literario.repositories.PostRepository;
 import com.blog.blog_literario.repositories.UserRepository;
+import com.blog.blog_literario.services.general.ImageStorageService;
 import com.blog.blog_literario.services.general.PostService;
 import com.blog.blog_literario.utils.SlugUtils;
 
@@ -30,22 +25,22 @@ import com.blog.blog_literario.utils.SlugUtils;
 public class PostServiceImpl implements PostService {
 
     @Autowired
-    private PostRepository postRepository; // Inyección del Repositorio de Posts
+    private PostRepository postRepository;
     @Autowired
-    private UserRepository userRepository; // Inyección del Repositorio de Usuarios
+    private UserRepository userRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ImageStorageService imageStorageService;
+
 
     @Override
     public Post createPost(postRequestDTO dto, MultipartFile image) {
-        // Obtener email del usuario
         String emailAuthor = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        // Usuario creador del post
         User author = userRepository.findByEmail(emailAuthor)
                 .orElseThrow(() -> new ResourceNotFoundException("Usuario no encontrado con correo: " + emailAuthor));
 
-        // Categoria del post
         Category category = categoryRepository.findById(dto.categoryId())
                 .orElseThrow(
                         () -> new ResourceNotFoundException("Categoria no encontrada con ID: " + dto.categoryId()));
@@ -55,31 +50,18 @@ public class PostServiceImpl implements PostService {
 
         // Crear nuevo post
         Post newPost = new Post(dto.title(), dto.content(), dto.estatus(), postSlug, author, category);
+
         newPost.setCreatedAt(LocalDateTime.now());
         newPost.setUpdatedAt(LocalDateTime.now());
 
-        if (image != null && !image.isEmpty()) {
-            // Generar el nombre del archivo a partir de un identificador aleatorio
-            // y el nombre original del archivo
-            String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
-            Path imagePath = Paths.get("ImgTest").resolve(fileName);
-            try {
-                // Crea la carpeta si no existe
-                Files.createDirectories(imagePath.getParent());
-                // param1: Flujo de datos del archivo
-                // param2: Copia el contenido de imagePath
-                // param3: Si existe un archivo con el mismo nombre lo sobreescribe
-                Files.copy(image.getInputStream(), imagePath, StandardCopyOption.REPLACE_EXISTING);
-                // Guarda la ruta relativa
-                newPost.setCoverImage("/ImgTest/" + fileName);
-            } catch (IOException e) {
-                throw new RuntimeException("Error al guardar la imagen" + e);
-            }
-        } else {
-            newPost.setCoverImage("/ImgTest/" + "default-image.png"); // Ruta por defecto si no se proporciona una imagen
+        if(image != null && !image.isEmpty()){
+            String imageUrl = imageStorageService.saveImage(image);
+            newPost.setCoverImage(imageUrl);
+        }else{
+            //Default image
+            newPost.setCoverImage("/api/images/default-image.png");
         }
 
-        // Guardar y Retornar
         return postRepository.save(newPost); // 201: Creado exitosamente
     }
 
@@ -106,26 +88,23 @@ public class PostServiceImpl implements PostService {
             existingPost.setCategory(category); // Guarda la nueva categoria
         }
 
-        return postRepository.save(existingPost); // Guarda y retorna el post actualizado
+        return postRepository.save(existingPost);
     }
 
     @Override
     public void deletePost(Integer id) {
-        if (!postRepository.existsById(id)) // Verifica la existencia del Post
+        if (!postRepository.existsById(id))
         {
-            throw new ResourceNotFoundException("No se encontró el Post con ID: " + id); // No existe el post: Lanza
-        }                                                                                         // Exepcion
+            throw new ResourceNotFoundException("No se encontró el Post con ID: " + id);
+        }    
 
-        postRepository.deleteById(id); // Existe el post: Elimina el Post
+        postRepository.deleteById(id); 
     }
 
     @Override
     public Post getPostById(Integer id) {
-        return postRepository.findById(id) // Verifica la existencia del Post
-                .orElseThrow(() -> new ResourceNotFoundException("No se encontró el Post con ID: " + id)); // No existe
-        // el post:
-        // Lanza
-        // Exepcion
+        return postRepository.findById(id) 
+                .orElseThrow(() -> new ResourceNotFoundException("No se encontró el Post con ID: " + id));
     }
 
     @Override
