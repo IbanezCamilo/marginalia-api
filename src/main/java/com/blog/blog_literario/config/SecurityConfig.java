04c333ce9ba12e8b5356ee90151a71f89f021cc6
ImCamilo17 <Camilopastrana41@gmail.com>
2026-02-16 23:06:12 -0500

    refactor(security): add EDITOR role and enable method security

--- src/main/java/com/blog/blog_literario/config/SecurityConfig.java
+++ src/main/java/com/blog/blog_literario/config/SecurityConfig.java
@@ -42,9 +42,15 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                //Public endpoints
-                .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/posts/**").authenticated()
+                //Public endpoints--------------
+                .requestMatchers(HttpMethod.GET, "/api/public/posts/**").permitAll()
+                //Authenticated author endpoints-----------------------
+                .requestMatchers("/api/me/posts/**").authenticated()
+                //--------------------------------------
+                //ADMIN endpoints-----------------------
+                .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                //--------------------------------------
+
                 .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
