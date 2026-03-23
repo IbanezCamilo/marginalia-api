b42d60c9693b361eca9d0ebb0900076021d3699a
ImCamilo17 <Camilopastrana41@gmail.com>
2026-03-22 22:33:45 -0500

    fix: add missing @RequestMapping to AdminPostController

--- src/main/java/com/blog/blog_literario/controllers/admin/AdminPostController.java
+++ src/main/java/com/blog/blog_literario/controllers/admin/AdminPostController.java
@@ -1,7 +1,9 @@
 package com.blog.blog_literario.controllers.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.blog_literario.dto.admin.AdminPostResponse;
 import com.blog.blog_literario.dto.admin.AdminStatusUpdateRequest;
@@ -18,11 +22,8 @@ import com.blog.blog_literario.services.posts.AdminPostModerationService;
 
 import jakarta.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
+@RequestMapping("/api/admin/posts")
 public class AdminPostController {
 
     private final AdminPostModerationService adminService;
